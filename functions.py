f900ccddfb81ac0b80c41f88a288fcf930957583
Androsov.AV <androsov.av@internet.ru>
2024-08-18 09:57:18 +0300

    Создана новая основная функция поиска по базе
    Она парсит базу без загрузки всей базы в
    оперативную память и работает для списка
    из деклистов
    	modified:   __pycache__/functions.cpython-312.pyc
    	modified:   functions.py
    	modified:   main.py
    	modified:   sources/data/lastElement.txt
    	modified:   sources/data/lastUpdateTime.txt

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import requests
 import time
 import mtg_parser
 import os
+import ijson
 
 # Функция, которая проверяет, удовлетворяет ли вход на турнир entry всем выставленным фильтрам
 def myFilter(filters, entry):
@@ -19,6 +20,69 @@ def myFilter(filters, entry):
                     and not any((card in set(entry['truedecklist']) for card in filters.excludeCardList)))
     except Exception:
         return False
+
+#   Основная функция парсинга базы данных. Принимает на вход фильтры и список из деклистов,
+#   возвращает количество побед, игр, винрейт и погрешность вычисления винрейта для
+#   каждого деклиста в списке.
+def parse(filters, cardList: list, dataFile: str):
+
+    #   Инициализируем словарь-результат и словарь, запоминающий присутствие
+    #   интересующих карт в деклисте
+    res = dict()
+    presenceDict = dict()
+    for i in range(len(cardList)):
+        res.update({i: {"wins": 0, "games": 0, "winrate": 0, "inaccuracy": 0}})
+        for card in cardList[i]:
+            presenceDict.update({card: False})
+
+    with open(dataFile, 'r') as file:
+        data = ijson.parse(file)
+        wins = 0
+        games = 0
+        colorID = ""
+        commander = ""
+        date = 0
+        for prefix, event, value in data:
+
+            #   Встречая оглавление нового деклиста проверяем, соответствовал ли последний
+            #   просмотренный всем требованиям. Если соответствовал, то учитываем его
+            #   количество побед и игр. Затем в любом случае обнуляем presenceDict
+            if event == "start_map":
+                for i in range(len(cardList)):
+                    if (any(color == colorID for color in filters.colors) or (filters.colors == {})
+                        and ((date >= filters.initialTime) or (filters.initialTime == 0))
+                        and ((date <= filters.finalTime) or (filters.finalTime == 0))
+                        and (any((card == commander) for card in filters.includeCommanders) or filters.includeCommanders == set())
+                        and (any((card != commander) for card in filters.excludeCommanders) or filters.excludeCommanders == set())
+                        and all((presenceDict[card] for card in filters.includeCardList))
+                        and not any((presenceDict[card] for card in filters.excludeCardList))
+                        and all(presenceDict[card] for card in cardList[i])):
+                            res[i].update({"wins": res[i]["wins"]+wins})
+                            res[i].update({"games": res[i]["games"]+games})
+                for card in presenceDict:
+                    presenceDict.update({card: False})
+                continue
+            if prefix.endswith('.wins'):
+                wins = value
+            elif prefix.endswith('.games'):
+                games = value
+            elif prefix.endswith('.colorID'):
+                colorID = value
+            elif prefix.endswith('.commander'):
+                commander = value
+            elif prefix.endswith('.date'):
+                date = value
+            elif prefix.endswith('.decklist.item'):
+                for card in presenceDict:
+                    if card == value:
+                        presenceDict.update({card: True})
+    
+    #   Зная количество побед и игр, подсчитываем винрейт и погрешность и записываем их в результат
+    for i in range(len(cardList)):
+        res[i].update({"winrate": res[i]["wins"]/res[i]["games"]})
+        res[i].update({"inaccuracy": res[i]["games"]**(-0.61)})
+
+    return res
 
 # Функция, которая считает погрешность измерения винрейта на основе количества игр в выборке
 def calculateInaccuracy(games):
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from classes import *
 from functions import *
 import json
-import ijson
 
-updateDatabase()
+# updateDatabase()
 
 # Открываем файл и загружаем фильтры поиска
 with open("sources/criteria/filters.json", 'r') as file:
@@ -11,13 +10,10 @@ with open("sources/criteria/filters.json", 'r') as file:
 
 filters = Filters(filtersJson)
 
-# Открываем файл и читаем его содержимое
-with open('sources/data/data.json', 'r') as file:
-    parser = ijson.parse(file)
-    i = 0
-    wins = 0
-    games = 0
-    for prefix, event, value in parser:
-        if prefix.endswith('games'):
-            games += value
-print(games)
+dataFile = 'sources/data/data.json'
+
+cardList = [set()]
+
+res = parse(filters, cardList, dataFile)
+print('winrate: ', round(res[0]['wins']/res[0]['games']*100, 2))
+print('inaccuracy: ', round(res[0]['inaccuracy']*100, 2))
